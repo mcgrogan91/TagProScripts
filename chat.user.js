1eafc96d477821357bfe26f4c813ec85c9cf5cef
Kyle McGrogan <mcgrogan91@gmail.com>
2016-09-26 22:41:30 -0400

    Create chat.user.js

---
+++ chat.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Mod Chat In Game
+// @namespace    http://tampermonkey.net/
+// @updateURL    https://github.com/mcgrogan91/TagProScripts/raw/master/chat.user.js
+// @version      0.1
+// @description  Lets you chat as a mod in-game
+// @author       You
+// @include       http://tangent.jukejuice.com*
+// @include       http://tagpro-*.koalabeast.com*
+// @grant        none
+// ==/UserScript==
+
+tagpro.keys.chatAsMod = [77];
