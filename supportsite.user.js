95fc09f7e86c61b56341704462749fff34b54a60
Kyle McGrogan <mcgrogan91@gmail.com>
2016-11-28 13:58:12 -0500

    Abuse of Report System button added
    Thanks Anne Frank!

--- supportsite.user.js
+++ supportsite.user.js
@@ -2,10 +2,11 @@
 // @name         Support Site Helper
 // @namespace    http://www.reddit.com/u/bizkut
 // @updateURL    https://github.com/mcgrogan91/TagProScripts/raw/master/supportsite.user.js
-// @version      1.1.3
+// @version      1.1.4
 // @description  Canned responses for the most common scenarios, displays ban information from the mod tools based on profileid, integrated wysiwyg markdown editor for easy formating of responses (includes a autosave feature of draft messages), fixes the text/url of those who enter full url's as there profileid rather then just the id itself while making those who enter there name as profile id display as plain text and not a clickable link...
 // @author       Bizkut
 // @contributor  OmicroN
+// @kinda helped Anne Frank
 // @include      https://support.koalabeast.com/*
 // @connect      koalabeast.com
 // @grant        GM_xmlhttpRequest
@@ -20,10 +21,11 @@ var appealName = 'Some Ball';
 // First commented out line is the template for adding more buttons.
 var buttons = {
     //'unique_value': ['Button Text', 'This text gets put into the text'],
+    'abuse_message' : ['Abuse of System', "Hi ##appealName##, \n\nYou were banned for filing numerous false reports for other players. As moderators, we check every report users make, and abusing the report system in this manner only prevents us from doing our jobs, not to mention leading others to be unjustly banned. Please refrain from repeatedly issuing false reports in the future, as it will result in a ban.\n\nIf you feel you were unjustly reported, feel free to come onto [IRC] (http://webchat.freenode.net/?channels=tpmods) and discuss your report there.\n\n*Please let us know if you have any further questions or concerns.*\n\n\Regards, "+modName],
     'standing_message': ['Good Standing',"Hi ##appealName##,\n\nYou're playing on a shared IP that has been banned. To bypass this, you need an account in good standing.\n\n**Good Standing Requirements:**\n\n - A registered account ✓\n - Minimum 5 hours playtime\n - Maximum of 2 reports in past 24 hours\n\n*Please let us know if you have any further questions or concerns.*\n\n\Regards, "+modName],
     'start_message': ['Start Format', "Hi ##appealName##,\n\nMessage Here\n\n*Please let us know if you have any further questions or concerns.*\n\n\Regards, "+modName],
     'afk_message': ['AFK Too Much', "Hi ##appealName##,\n\nYou were banned for receiving 8 reports within 24 hours. Most of these reports are for not moving for 30 seconds, and getting kicked by the AFK timer. Please try to stay active in-game, and click the exit button if you need to leave. Also, try not to switch tabs inbetween games, because you might end up in a game and not realize it!\n\n*Please let us know if you have any further questions or concerns.*\n\n\Regards, "+modName],
-    'chat_message': ['Offensive Chat', "Hi ##appealName##,\n\nYou were banned for your chat:\n\nChat Here!\n\n*Please let us know if you have any further questions or concerns.*\n\n\Regards, "+modName],
+    'chat_message': ['Offensive Chat', "Hi ##appealName##,\n\nYou were banned for your chat:\n\n[Chat Here!]\n\n*Please let us know if you have any further questions or concerns.*\n\n\Regards, "+modName],
     'no_account_message': ['No Profile ID', "Hi ##appealName##,\n\nCan you please tell us your account name or your profile ID? Without this we have no idea who you are.\n\nIf you don't have an account, the only way you can play on a banned IP is through an account in Good Standing.\n\n**Good Standing Requirements:**\n\n - A registered account\n - Minimum 5 hours playtime\n - Maximum of 2 reports in past 24 hours\n\n*Please let us know if you have any further questions or concerns.*\n\n\Regards, "+modName]
 };
 
